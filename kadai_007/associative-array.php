0ab6966027826a6f4daeeccb5600d3664170bc2d
Lin-QvQ <168858436+Lin-QvQ@users.noreply.github.com>
2024-08-04 17:16:46 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html lang="ja">
+ 
+  <head>
+    <meta charset="UTF-8">
+  </head>
+ 
+  <body>
+    <p>
+      <?php
+        $date = ['name' => 'onion','price' => 200,'weight' => 160];
+
+        print_r($date);
+      ?>
+    </p>
+  </body>
+
+</html>
